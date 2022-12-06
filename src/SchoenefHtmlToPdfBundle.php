34090d8b76d40b0a32d5b3cef6c886dcfe6ff8e4
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2022-12-06 16:09:15 +0100

    cleanup

--- src/SchoenefHtmlToPdfBundle.php
+++ src/SchoenefHtmlToPdfBundle.php
@@ -2,12 +2,9 @@
 
 namespace Schoenef\HtmlToPdfBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SchoenefHtmlToPdfBundle extends AbstractBundle
 {
-    public function build(ContainerBuilder $container) {
 
-    }
 }
