0e75e8f3566cee07f3820f4a07549ab82d87663f
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2022-12-06 15:46:03 +0100

    Fighting with the configuration

--- src/DependencyInjection/SchoenefHtmlToPdfExtension.php
+++ src/DependencyInjection/SchoenefHtmlToPdfExtension.php
@@ -8,11 +8,10 @@
 
 namespace Schoenef\HtmlToPdfBundle\DependencyInjection;
 
-
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -26,12 +25,7 @@ class SchoenefHtmlToPdfExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $container->setParameter('schoenef_html_to_pdf', $config);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yml');
     }
 
